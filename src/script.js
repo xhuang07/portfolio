b8cff63d9945d2752ae88f83bbb86a6348e90139
Xingwei <huangxingwei95@gmail.com>
2022-08-06 23:48:41 -0700

    Adjustments to intro page

--- src/script.js
+++ src/script.js
@@ -163,4 +163,13 @@ easyScrollDots({
 });
 
 
-// Local time 
+//Progress bar
+const scrollProgress = document.getElementById('scroll-progress');
+const height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener('scroll', () => {
+  const scrollTop =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+});
