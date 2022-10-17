dd1338253792891642ab252e8cd82dd9a6c3b592
Xingwei <huangxingwei95@gmail.com>
2022-10-16 22:41:26 -0700

    Add new template

--- src/script.js
+++ src/script.js
@@ -231,14 +231,14 @@ window.addEventListener('scroll', () => {
 
 //Top Navigation Active effect
 // Add active class to the current button (highlight it)
-var header = document.getElementById("topNav");
-var btns = header.getElementsByClassName("nav__link");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  if (current.length > 0) { 
-    current[0].className = current[0].className.replace(" active", "");
-  }
-  this.className += " active";
-  });
-}
+// var header = document.getElementById("topNav");
+// var btns = header.getElementsByClassName("nav__link");
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//   var current = document.getElementsByClassName("active");
+//   if (current.length > 0) { 
+//     current[0].className = current[0].className.replace(" active", "");
+//   }
+//   this.className += " active";
+//   });
+// }
