383705713c20695c335ccf894ff09c00093f4686
Xingwei <huangxingwei95@gmail.com>
2022-10-19 22:11:25 -0700

    Added animation

--- src/scrolly.js
+++ src/scrolly.js
@@ -16,4 +16,19 @@ scroller
     // { element, index, direction }
   });
 
-  
+
+  function scroll(n, offset, func1, func2){
+    return new Waypoint({
+      element: document.getElementById(n),
+      handler: function(direction) {
+        direction == 'down' ? func1() : func2();
+      },
+      //start 75% from the top of the div
+      offset: offset
+    });
+  };
+
+
+new scroll('step1', '75%', f3, f4);
+new scroll('step2', '75%', f4, f5);
+new scroll('step3', '75%', f5, f3);
